15b431d57e5cbc589f94b56c78f468da726fa6b4
Alexander Vorobev <3048453@gmail.com>
2025-02-12 23:36:01 +0300

    #3 add jdbc adapter

---
+++ passenger-transportation-service/passenger-transportation-jdbc-adapter/src/main/java/org/service/jdbc/TranspotationJdbcAdapter.java
@@ -0,0 +1,7 @@
+package org.service.jdbc;
+
+import org.service.output_port.TransportationServiceOutputPort;
+import org.springframework.jdbc.object.MappingSqlQuery;
+
+public class TranspotationJdbcAdapter implements TransportationServiceOutputPort {
+}
