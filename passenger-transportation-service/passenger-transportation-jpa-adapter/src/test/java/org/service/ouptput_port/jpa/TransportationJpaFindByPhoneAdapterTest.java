dada989bfd5e78799604c6ccef821bf95e5d0794
Alexander Vorobev <3048453@gmail.com>
2025-02-26 19:18:28 +0300

    #19 add tests classes

---
+++ passenger-transportation-service/passenger-transportation-jpa-adapter/src/test/java/org/service/ouptput_port/jpa/TransportationJpaFindByPhoneAdapterTest.java
@@ -0,0 +1,12 @@
+package org.service.ouptput_port.jpa;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TransportationJpaFindByPhoneAdapterTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
