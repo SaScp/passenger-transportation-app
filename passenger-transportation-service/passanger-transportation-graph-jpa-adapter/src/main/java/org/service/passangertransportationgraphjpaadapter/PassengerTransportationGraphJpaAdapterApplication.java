a214ecf8e1e49ce4e4a7247ff0be67e9f9f3a6aa
Alexander Vorobev <3048453@gmail.com>
2025-03-03 23:51:18 +0300

    #38 init graph version

---
+++ passenger-transportation-service/passanger-transportation-graph-jpa-adapter/src/main/java/org/service/passangertransportationgraphjpaadapter/PassengerTransportationGraphJpaAdapterApplication.java
@@ -0,0 +1,13 @@
+package org.service.passangertransportationgraphjpaadapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassengerTransportationGraphJpaAdapterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PassengerTransportationGraphJpaAdapterApplication.class, args);
+    }
+
+}
---
+++ passenger-transportation-service/passenger-transportation-jpa-graph-adapter/src/main/java/org/service/output_purt/model/Route.java
@@ -0,0 +1,4 @@
+package org.service.output_purt.model;
+
+public class Route {
+}
