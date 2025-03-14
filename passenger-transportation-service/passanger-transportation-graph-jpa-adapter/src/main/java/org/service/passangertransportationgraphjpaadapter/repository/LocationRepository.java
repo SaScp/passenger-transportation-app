8226b6bf944b8b98350cc5d9b207b82d6c79d82a
Alexander Vorobev <3048453@gmail.com>
2025-03-15 00:41:55 +0300

    #52 delete integration module

--- passenger-transportation-service/passanger-transportation-graph-jpa-adapter/src/main/java/org/service/passangertransportationgraphjpaadapter/repository/LocationRepository.java
+++
@@ -1,9 +0,0 @@
-package org.service.passangertransportationgraphjpaadapter.repository;
-
-import org.service.passangertransportationgraphjpaadapter.model.Location;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LocationRepository extends JpaRepository<Location, String> {
-}
