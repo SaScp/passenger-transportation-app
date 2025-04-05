90e658953fd9e95f4c6294cc9e974b7c2fa098d4
Alexander Vorobev <3048453@gmail.com>
2025-04-05 16:53:35 +0300

    #68 add compFuture in core

--- passenger-transportation-frontend/src/api.js
+++ passenger-transportation-frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const api = axios.create({
-    baseURL: "http://localhost:9000/dev/api/v1",
+    baseURL: "http://localhost:8080/api/v1",
 });
 
 export const findRoutesByParams = async (params) => {
